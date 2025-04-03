63103ffd738fa6b2bc60c925e9a64ba545597514
Sarthak Kumar Behera <sarthakbehera09@gmail.com>
2025-04-03 22:24:15 +0530

    Minor fix of toaster

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,21 @@
 import { MainContent } from "./components/MainContent";
 import { Sidebar } from "./components/Sidebar";
 import { Topbar } from "./components/Topbar";
+import { Toaster } from "./components/ui/sonner";
 import userAvatar from "./icons/user.png";
 
 function App() {
   return (
-    <div className="h-screen flex flex-col ">
-      <Topbar name="Lorem Ips" role="Manager" profileImg={userAvatar} />
-      <div className="flex flex-1 bg-[#F5F4F4]">
-        <Sidebar />
-        <MainContent />
+    <>
+      <div className="h-screen flex flex-col ">
+        <Topbar name="Lorem Ips" role="Manager" profileImg={userAvatar} />
+        <div className="flex flex-1 bg-[#F5F4F4]">
+          <Sidebar />
+          <MainContent />
+        </div>
       </div>
-    </div>
+      <Toaster />
+    </>
   );
 }
 
